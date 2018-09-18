87c18e618b5cfe38e4b9988b0a0e2f819bbba7d9
Armin Spahic <armin.spaha@gmail.com>
2018-09-18 13:14:11 +0200

    added new files and updated sql script

--- sql_scripts/angularjs_user_list.sql
+++ sql_scripts/angularjs_user_list.sql
@@ -1,79 +1,53 @@
-DROP DATABASE  IF EXISTS `angularjs_user_list`;
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: angularjs_user_list
+-- ------------------------------------------------------
+-- Server version	5.7.23-0ubuntu0.18.04.1
 
-CREATE DATABASE  IF NOT EXISTS `angularjs_user_list`;
-USE `angularjs_user_list`;
-
---
--- Table structure for table `profile`
---
-
-DROP TABLE IF EXISTS `profile`;
-CREATE TABLE `profile` (
-  `username` varchar(50) NOT NULL,
-  `password` char(68) NOT NULL,
-  `enabled` tinyint(1) NOT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `profile`
---
--- NOTE: The passwords are encrypted using BCrypt
---
--- A generation tool is avail at: https://www.dailycred.com/article/bcrypt-calculator
---
--- Default passwords are: test123
---
-
-INSERT INTO `profile` 
-VALUES 
-('employee','{bcrypt}$2a$04$Db5KWqKHpsAYD9KFU8Xp2OIdyP3OaozRymW9refvUiimzSET3KuxC',1),
-('manager','{bcrypt}$2a$04$4NK1N41BpUV4mGWEA9/CreFlsaDqfqSAKhgYt1D/H5LVi3gBne/b6',1),
-('admin','{bcrypt}$2a$04$oXhqrK.YKDhc6EbpOz.MXOP5LQb.S8Uc27fv1GV7jVEI8sqi7yWfe',1);
-
-
---
--- Table structure for table `authorities`
---
-
-DROP TABLE IF EXISTS `authorities`;
-CREATE TABLE `authorities` (
-  `username` varchar(50) NOT NULL,
-  `authority` varchar(50) NOT NULL,
-  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
-  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `profile` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `authorities`
---
-
-INSERT INTO `authorities` 
-VALUES 
-('employee','ROLE_EMPLOYEE'),
-('manager','ROLE_EMPLOYEE'),
-('manager','ROLE_MANAGER'),
-('admin','ROLE_EMPLOYEE'),
-('admin','ROLE_ADMIN');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` VALUES 
-(1,'Edin','Dzeko','edin@someemail.com'),
-(2,'Meho','Kodro','meho@someemail.com');
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Uncle','Bob','bob@email.com'),(2,'Steve','Jobs','steve@email.com');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-18 12:52:02
